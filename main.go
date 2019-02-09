ab4e80657818d2f14531f9e328ff90bfe8959c6b
Montana Flynn <montana949@gmail.com>
2019-02-10 03:02:53 +0700

    Add svg template and comments

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,8 +16,9 @@ import (
 
 func main() {
 
-	// get and parse yaml
-	req, err := http.Get("https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml")
+	// get and decode yaml
+	baseURL := "https://raw.githubusercontent.com"
+	req, err := http.Get(baseURL + "/github/linguist/master/lib/linguist/languages.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,22 +29,27 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// generate svgs and readme
 	readme := "# Github Language Colors\n\n"
 	link := "[![](./svgs/%s.svg)](%s)\n"
 
+	// sort languages by name
 	keys := make([]string, 0)
 	for k := range data {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 
-	// find langs with colors
+	// find languages with colors
 	for _, lang := range keys {
 		meta := data[lang]
 		if meta["color"] != nil {
 			color, ok := meta["color"].(string)
 			if ok {
+
 				url := fmt.Sprintf("https://github.com/trending?l=%s", lang)
+
+				// check if color is light or dark to determine text color
 				c, err := colorful.Hex(color)
 				if err != nil {
 					log.Fatal(err)
@@ -53,11 +61,32 @@ func main() {
 					textColor = "#000"
 				}
 
-				// create svg
+				// create svg images
 				cleaned := strings.Replace(lang, " ", "-", -1)
 				cleaned = strings.Replace(cleaned, "'", "-", -1)
-				svg := fmt.Sprintf(`<svg width="600" height="50" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="596" height="46" style="fill:%s"/><text x="50%%" y="50%%" font-size="18" text-anchor="middle" alignment-baseline="middle" font-family="monospace, sans-serif" fill="%s">%s</text></svg>`, color, textColor, lang)
-				err = ioutil.WriteFile("./svgs/"+cleaned+".svg", []byte(svg), 0644)
+
+				t, err := template.ParseFiles("./svg.tmpl")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				var svg bytes.Buffer
+
+				templateData := struct {
+					LangName  string
+					LangColor string
+					TextColor string
+				}{
+					lang,
+					color,
+					textColor,
+				}
+				err = t.Execute(&svg, templateData)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				err = ioutil.WriteFile("./svgs/"+cleaned+".svg", svg.Bytes(), 0644)
 				if err != nil {
 					log.Fatal(err)
 				}
@@ -69,12 +98,14 @@ func main() {
 				// encode any single quotes
 				lang = strings.Replace(lang, "'", "&apos;", -1)
 				url = strings.Replace(url, "'", "&apos;", -1)
+
+				// add language to readme
 				readme += fmt.Sprintf(link, cleaned, url)
 			}
 		}
 	}
 
-	// generate README.md
+	// create README.md
 	err = ioutil.WriteFile("./README.md", []byte(readme), 0644)
 	if err != nil {
 		log.Fatal(err)
